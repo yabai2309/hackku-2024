1a92491c118dac6f748bf7c14c63cc8def8ff09e
yabai2309 <hpa2309@gmail.com>
2023-11-11 14:36:49 -0600

    contact & sponsor pages 1

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -4,12 +4,76 @@
 
 <div>
   <div class="section contact-container" id="contact">
-    <h2>Contact</h2>
+    <h2 id="header">Meet the team</h2>
+    <div class="contact_gallery">
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact1" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact2" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact3" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact4" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact5" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact6" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact7" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact8" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact9" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact10" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact11" id="portrait">
+    </div>
+    <div class="contact_images">
+      <img src="images/20.png" alt="contact12" id="portrait">
+    </div>
   </div>
-</div>
+    </div>
+  </div>
 
 <style>
   .contact-container {
     background-color: var(--green-shad)
+  }
+  .contact_images {
+  width: 225px; 
+  height: 300px;
+  overflow: hidden;
+  border-radius: 50%; 
+  border: 5px solid #FFCD59;
+  }
+  
+  .contact_images img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  }
+  .contact_gallery {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap; 
+    justify-content:center;
+    row-gap: 100px;
+    column-gap: 50px;
+    margin: 20px;
+    grid-template-columns: repeat(4, 1fr);
+  }
+  #header {
+    color: white;
   }
 </style>
--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -5,11 +5,62 @@
 <div>
   <div class="section sponsors-container" id="sponsors">
     <h2>Sponsors</h2>
+    <p id="sponsors-description">Interested in sponsoring HackKU24?
+    <br>Contact us at sponsorship.hackku@gmail.com</p>
+    <h3 id="sponsors-divider">Presented by</h3>
+    <div class="sponsors-tier">
+      <img src="images/20.png" alt="sponsor1">
+    </div>
+    <h3 id="sponsors-divider">PETA Tier</h3>
+    <div class="sponsors-tier">
+      <img src="images/20.png" alt="sponsor2">
+      <img src="images/20.png" alt="sponsor3">
+      <img src="images/20.png" alt="sponsor4">
+    </div>
+    <h3 id="sponsors-divider">TERA Tier</h3>
+    <div class="sponsors-tier" alt="sponsor5"> 
+      <img src="images/20.png" alt="sponsor6">
+      <img src="images/20.png" alt="sponsor7">
+      <img src="images/20.png" alt="sponsor8">
+    </div>
+    <h3 id="sponsors-divider">GIGA Tier</h3>
+    <div class="sponsors-tier" alt="sponsor9">
+      <img src="images/20.png" alt="sponsor10">
+      <img src="images/20.png" alt="sponsor11">
+      <img src="images/20.png" alt="sponsor12">
+      <img src="images/20.png" alt="sponsor13">
+    </div>
+    <h3 id="sponsors-divider">MEGA Tier</h3>
+    <div class="sponsors-tier">  
+      <img src="images/20.png" alt="sponsor14">
+      <img src="images/20.png" alt="sponsor15">
+      <img src="images/20.png" alt="sponsor16">
+      <img src="images/20.png" alt="sponsor17">
+    </div>
   </div>
 </div>
 
 <style>
   .sponsors-container {
     background-color: var(--off-white)
+  }
+  #sponsors-description {
+    font-size: 20px;
+    text-align: center;
+    margin-top: -30px;
+  }
+  #sponsors-divider {
+    text-align: center;
+    margin-top: 100px;
+    margin-bottom: 30px;
+  }
+  .sponsors-tier {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap; 
+    justify-content:center;
+    row-gap: 10px;
+    column-gap: 10px;
+    margin: 20px;
   }
 </style>
